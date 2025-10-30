748d71272c018c2e827481311122cbdbe76adbbd
trivenibanavatu-aiml <triveni.banavatu@gmail.com>
2025-10-30 14:31:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.text("WELCOME TO TEEGALA KRISHNA REDDY ENGINEERING COLLEGE")
+st.image(image="./images/4.jpeg",width=500)
+st.image(image="./images/5.jpeg",width=500)
+st.text("FDP on Modern Web Development & AI integration")
